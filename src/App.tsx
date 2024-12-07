976f88b999e0466afacd451b03d7dfc8cdcc293f
couvq <quentincouvelaire@gmail.com>
2024-12-07 10:08:21 -0500

    setup barebones react-intl

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormattedMessage, IntlProvider } from "react-intl";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Translated messages in French with matching IDs to what you declared
+const messagesInFrench = {
+  greeting: "Bonjour je m'appele {name}",
+};
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <IntlProvider messages={messagesInFrench} locale="fr" defaultLocale="en">
+      <p>
+        <FormattedMessage
+          id="greeting"
+          defaultMessage="Bonjour je m'appele {name}"
+          values={{ name: "Quentin" }}
+        />
       </p>
-    </>
-  )
-}
+    </IntlProvider>
+  );
+};
 
-export default App
+export default App;
