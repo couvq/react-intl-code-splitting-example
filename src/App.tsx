800c5a7c853af85d4f4af9f454749662667ab5b3
couvq <quentincouvelaire@gmail.com>
2024-12-07 10:43:51 -0500

    adding locale specific string files with dynamic imports for react-intl

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
+import { Suspense, useEffect, useState } from "react";
 import { FormattedMessage, IntlProvider } from "react-intl";
 
-// Translated messages in French with matching IDs to what you declared
-const messagesInFrench = {
-  greeting: "Bonjour je m'appele {name}",
+/**
+ * Dynamically import translated files for a given local, only when the translations are needed.
+ * @param locale users selected locale
+ * @returns json file containing translated strings for the given locale
+ */
+const getMessagesByLocale = async (locale: string) => {
+  return await import(`./translations/strings-${locale}.json`);
 };
 
 const App = () => {
+  const [messages, setMessages] = useState({});
+  const locale = "fr";
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('content loading...')
+    }, 2000);
+    getMessagesByLocale(locale)
+      .then((messages) => setMessages(messages))
+      .catch(() =>
+        console.error(`Error loading messages for locale: ${locale}`)
+      );
+  }, []);
+
   return (
-    <IntlProvider messages={messagesInFrench} locale="fr" defaultLocale="en">
-      <p>
-        <FormattedMessage
-          id="greeting"
-          defaultMessage="Bonjour je m'appele {name}"
-          values={{ name: "Quentin" }}
-        />
-      </p>
-    </IntlProvider>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <IntlProvider messages={messages} locale="fr" defaultLocale="en">
+        <p>
+          <FormattedMessage
+            id="greeting"
+            defaultMessage="Bonjour je m'appele {name}"
+            values={{ name: "Quentin" }}
+          />
+        </p>
+      </IntlProvider>
+    </Suspense>
   );
 };
 
